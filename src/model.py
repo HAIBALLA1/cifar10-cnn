f3c05c4f8fe18143ce44fd0080f3754b05af7345
farouk <haiballaelvarough@gmail.com>
2026-01-23 19:06:19 +0100

    bug

--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 class  DenoisingAutoencoder(nn.Module):
 
-    def __init__(self, encoder, decoder):
+    def __init__(self):
         super().__init__()
 
         self.encoder = nn.Sequential(
@@ -15,23 +15,24 @@ class  DenoisingAutoencoder(nn.Module):
 
             nn.MaxPool2d(2),
 
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),  # (B, 64, 16, 16)
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
 
             nn.MaxPool2d(2),
         )
 
         self.decoder = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode="nearest"),  # (B, 64, 16, 16)
-            nn.Conv2d(64, 32, kernel_size=3, padding=1),  # (B, 32, 16, 16)
+
+            nn.Upsample(scale_factor=2, mode="nearest"),
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
 
-            nn.Upsample(scale_factor=2, mode="nearest"),  # (B, 32, 32, 32)
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),  # (B, 32, 32, 32)
+            nn.Upsample(scale_factor=2, mode="nearest"),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(32, 3, kernel_size=3, padding=1),  # (B, 3, 32, 32)
-            nn.Tanh(),  # sortie dans [-1, 1]
+            nn.Conv2d(32, 3, kernel_size=3, padding=1),
+            nn.Tanh(),
 
         )
 
