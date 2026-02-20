173db7ba13e9d21403bcabd149abff2adab3841a
farouk <haiballaelvarough@gmail.com>
2026-02-20 01:10:21 +0100

    refactor(data): migrate CIFAR10 DataLoader to Hydra config-based setup

--- src/data.py
+++ src/data.py
@@ -1,13 +1,11 @@
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-
 CIFAR10_CLASSES = [
     "airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck"
 ]
 
 def get_transforms(use_augmentation=True):
-
     mean = (0.4914, 0.4822, 0.4465)
     std = (0.2023, 0.1994, 0.2010)
 
@@ -31,32 +29,39 @@ def get_transforms(use_augmentation=True):
 
     return train_tf, test_tf
 
-def get_cifar10_loaders(
-    data_dir="./data",
-    batch_size=128,
-    num_workers=2,
-    use_augmentation=True):
 
-    train_tf, test_tf = get_transforms(use_augmentation)
-    train_ds = datasets.CIFAR10(root=data_dir, train=True, download=True, transform=train_tf)
-    test_ds = datasets.CIFAR10(root=data_dir, train=False, download=True, transform=test_tf)
+def get_loaders(cfg):
+    """Hydra entrypoint: tout vient de cfg.data"""
+    dcfg = cfg.data
+
+    train_tf, test_tf = get_transforms(dcfg.use_augmentation)
+
+    train_ds = datasets.CIFAR10(
+        root=dcfg.data_dir,
+        train=True,
+        download=True,
+        transform=train_tf
+    )
+    test_ds = datasets.CIFAR10(
+        root=dcfg.data_dir,
+        train=False,
+        download=True,
+        transform=test_tf
+    )
 
     train_loader = DataLoader(
         train_ds,
-        batch_size=batch_size,
+        batch_size=dcfg.batch_size,
         shuffle=True,
-        num_workers=num_workers,
+        num_workers=dcfg.num_workers,
         pin_memory=True,
     )
     test_loader = DataLoader(
         test_ds,
-        batch_size=batch_size,
+        batch_size=dcfg.batch_size,
         shuffle=False,
-        num_workers=num_workers,
+        num_workers=dcfg.num_workers,
         pin_memory=True,
     )
 
     return train_loader, test_loader
-
-
-
